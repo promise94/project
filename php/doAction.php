e56c7b70f026d1368842bf7e71e777928095b100
xiaowei9495 <promise9495@163.com>
2017-06-24 15:33:20 +0800

    2017.6.24

--- php/doAction.php
+++ php/doAction.php
@@ -1,48 +1,47 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username  = $_POST['username'];
+$password  = $_POST['password'];
 $password1 = $_POST['password1'];
-$email = $_POST['email'];
-$fav = $_POST['fav'];
-$verfy = trim($_POST['verfy']);
-$verfy1 = trim($_POST['verfy1']);
+$email     = $_POST['email'];
+$fav       = $_POST['fav'];
+$verfy     = trim($_POST['verfy']);
+$verfy1    = trim($_POST['verfy1']);
 
 $redirectUrl = '<a href="注册.php">请重新注册</a>';
 
-
 //验证用户名
 $charname = $username{0};
-$ascname = ord($charname);
+$ascname  = ord($charname);
 //大写字母65~90 小写字母97~122
-if (!($ascname >= 65 && $ascname <= 90|| $ascname >=97 && $ascname <=122)) {
-    exit('用户名输入错误，'.$redirectUrl);
+if (!($ascname >= 65 && $ascname <= 90 || $ascname >= 97 && $ascname <= 122)) {
+    exit('用户名输入错误，' . $redirectUrl);
 }
 //验证用户名长度
 if (strlen($username) < 6 || strlen($username) > 10) {
-    exit('用户名输入错误，'.$redirectUrl);
+    exit('用户名输入错误，' . $redirectUrl);
 }
 //密码验证
 $pwdlen = strlen($password);
 
 if ($pwdlen == 0) {
-    exit('密码不能为空，'.$redirectUrl);
+    exit('密码不能为空，' . $redirectUrl);
 }
 
 if ($pwdlen < 6 || $pwdlen > 10) {
-    exit('密码输入错误，'.$redirectUrl);
+    exit('密码输入错误，' . $redirectUrl);
 }
 
-if (strcmp($password, $password1) !==0) {
-    exit('两次密码输入不一致，'.$redirectUrl);
+if (strcmp($password, $password1) !== 0) {
+    exit('两次密码输入不一致，' . $redirectUrl);
 }
 
 //验证码校验
-if (strcasecmp($verfy,$verfy1) !== 0) {
-    exit('验证码输入不正确，'.$redirectUrl);
+if (strcasecmp($verfy, $verfy1) !== 0) {
+    exit('验证码输入不正确，' . $redirectUrl);
 }
 
-$love = implode('.', $fav);
+$love     = implode('.', $fav);
 $password = md5(md5($password));
 $userInfo = <<<"info"
 <table>
@@ -62,4 +61,3 @@ $userInfo = <<<"info"
 info;
 
 echo $userInfo;
-?>
